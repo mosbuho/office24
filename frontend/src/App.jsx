93babad42abca8d481df61c9b040f2b1e5b59727
mosbuho <1198kguz@gmail.com>
2024-07-24 12:50:45 +0900

    App Route 설정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Main from './pages/Main.jsx';
+import ManagerMain from './pages/ManagerMain.jsx';
+import AdminMain from './pages/AdminMain.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/manager" element={<ManagerMain />} />
+        <Route path="/admin" element={<AdminMain />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/AdminMain.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AdminMain() {
+  return (
+    <div>
+      <h1>Admin Main Page</h1>
+    </div>
+  );
+}
+
+export default AdminMain;
