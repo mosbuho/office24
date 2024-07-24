02a8b4e281c6c6f81b83f8bf3d428172c7a3d6f2
mosbuho <1198kguz@gmail.com>
2024-07-24 10:11:52 +0900

    user sql 삭제

--- office24-user.sql
+++
@@ -1,7 +0,0 @@
-alter session set "_ORACLE_SCRIPT" = true;
-
-create user office24 identified by office24
-	default tablespace users
-	temporary tablespace temp;
-
-grant connect,resource,dba to office24;
